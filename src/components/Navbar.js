245b562e282c583cf5ae1bc3ef301e4fac22b928
Ananya Pathak <anyalko043@gmail.com>
2025-10-04 03:39:59 +0530

    Adjust fonts to improve overall UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,8 @@ import { FaGithub } from "react-icons/fa";
 export default function Navbar() {
   return (
     <div>
-      <div className="flex items-center justify-center relative p-6">
-        <p className="text-2xl font-bold text-white absolute left-[47%] transform -translate-x-1/2 mt-4">
+      <div className="flex items-center justify-center relative p-10">
+        <p className="text-4xl font-bold text-white absolute left-[47%] transform -translate-x-1/2 mt-4">
           veri<span className="text-red-500">Fy</span>
         </p>
         <a
@@ -13,7 +13,7 @@ export default function Navbar() {
           rel="noopener noreferrer"
           className="ml-auto mr-15 mt-4"
         >
-          <FaGithub className="text-white w-6 h-6 hover:text-red-500" />
+          <FaGithub className="text-white w-7 h-7 hover:text-red-500" />
         </a>
       </div>
     </div>
